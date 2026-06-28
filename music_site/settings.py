91772c5b97ec8d5c8a8d5aaf89374390581b61ff
Leks-228 <sasaonackij789@gmail.com>
2026-06-29 01:54:21 +0400

    fix csrf

--- music_site/settings.py
+++ music_site/settings.py
@@ -89,11 +89,10 @@ WSGI_APPLICATION = 'music_site.wsgi.application'
 # DATABASE (Railway PostgreSQL)
 # =========================
 
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 if os.environ.get("DATABASE_URL"):
